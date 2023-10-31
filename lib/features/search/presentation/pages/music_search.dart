3469721dec9e6eaf850971eafadcc6e4517aa92e
Sourav Barman <Sourav.Barman@brainstation-23.com>
2023-10-31 14:18:25 +0600

    feat: Dark theme

--- lib/features/search/presentation/pages/music_search.dart
+++ lib/features/search/presentation/pages/music_search.dart
@@ -2,9 +2,11 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:core/core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:input_form_field/input_form_field.dart';
+import 'package:music_app/core/constants/app_color.dart';
 import 'package:music_app/core/constants/text_style.dart';
 import 'package:music_app/features/search/presentation/riverpod/music_search_provider.dart';
 import 'package:music_app/features/search/presentation/widgets/search_item.dart';
@@ -43,24 +45,33 @@ class _MusicSearchState extends ConsumerState<MusicSearch> {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
-        backgroundColor: const Color(0xffffffff),
+        backgroundColor: bgColor,
         appBar: AppBar(
-          backgroundColor: const Color(0xffffffff),
+          backgroundColor: bgColor,
           title: Text(
             'Find Your Music',
             style: AppTextStyle.textStyleOne(
-              Colors.black,
+              Colors.white,
               20,
               FontWeight.w500,
             ),
           ),
           centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+              HapticFeedback.mediumImpact();
+            },
+            icon: const Icon(Icons.arrow_back_ios_new_rounded),
+            iconSize: 22,
+            color: Colors.white,
+          ),
         ),
         body: SingleChildScrollView(
           child: Column(
             children: [
               Container(
-                color: const Color(0xffffffff),
+                color: bgColor,
                 height: 0.20.sh,
                 child: Column(
                   children: [
@@ -70,7 +81,7 @@ class _MusicSearchState extends ConsumerState<MusicSearch> {
                       width: double.infinity,
                       child: DefaultTextStyle(
                         style: const TextStyle(
-                          color: Colors.black87,
+                          color: Colors.white,
                           fontSize: 20,
                           fontWeight: FontWeight.bold,
                         ),
@@ -80,9 +91,11 @@ class _MusicSearchState extends ConsumerState<MusicSearch> {
                             FadeAnimatedText('Immerse in symphonies of sound.'),
                             FadeAnimatedText('Tune in anytime, anywhere.'),
                             FadeAnimatedText(
-                                'Music speaks the language of the soul.',),
+                              'Music speaks the language of the soul.',
+                            ),
                             FadeAnimatedText(
-                                'Every beat, every note, in your hand.',),
+                              'Every beat, every note, in your hand.',
+                            ),
                             FadeAnimatedText('Let music move you.'),
                             FadeAnimatedText('Your groove, your playlists.'),
                             FadeAnimatedText('Classic hits to fresh releases.'),
@@ -91,7 +104,8 @@ class _MusicSearchState extends ConsumerState<MusicSearch> {
                             FadeAnimatedText('Music, part of your daily life.'),
                             FadeAnimatedText('Unlock the world of music.'),
                             FadeAnimatedText(
-                                'Your musical journey starts here.',),
+                              'Your musical journey starts here.',
+                            ),
                             FadeAnimatedText('Create your musical story.'),
                             FadeAnimatedText('Dance to your playlist.'),
                             FadeAnimatedText('Share your music passions.'),
@@ -150,11 +164,11 @@ class _MusicSearchState extends ConsumerState<MusicSearch> {
                 height: 0.70.sh,
                 width: double.infinity,
                 decoration: const BoxDecoration(
-                  color: Color(0xffffffff),
+                  color: bgColor,
                 ),
                 child: Container(
                   decoration: const BoxDecoration(
-                    color: Color(0xfff6f7fb),
+                    color: bgColor,
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30),
                       topRight: Radius.circular(30),
